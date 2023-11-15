3e1a730028939d25d4805b841f33204f9424c41f
Francesco Galetta <f.galetta@reply.it>
2023-11-15 15:23:41 +0100

    Fix_V8_Path_Traversal

--- src/com/notsecurebank/util/ServletUtil.java
+++ src/com/notsecurebank/util/ServletUtil.java
@@ -277,4 +277,34 @@ public class ServletUtil {
         request.getSession().setAttribute("specialPrizeCode", specialPrizeCode);
         return specialPrizeCode;
     }
+
+    public static void failIfDirectoryTraversal(String relativePath){
+        File file = new File(relativePath);
+
+        if (file.isAbsolute())
+        {
+            throw new RuntimeException("Directory traversal attempt - absolute path not allowed");
+        }
+
+        String pathUsingCanonical;
+        String pathUsingAbsolute;
+        try
+        {
+            pathUsingCanonical = file.getCanonicalPath();
+            pathUsingAbsolute = file.getAbsolutePath();
+        }
+        catch (IOException e)
+        {
+            throw new RuntimeException("Directory traversal attempt?", e);
+        }
+
+
+        // Require the absolute path and canonicalized path match.
+        // This is done to avoid directory traversal 
+        // attacks, e.g. "1/../2/" 
+        if (! pathUsingCanonical.equals(pathUsingAbsolute))
+        {
+            throw new RuntimeException("Directory traversal attempt?");
+        }
+    }
 }
