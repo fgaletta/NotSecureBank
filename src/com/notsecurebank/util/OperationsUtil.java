bc8899dbbf724c79b98d30903415ebd71e516c89
Francesco Galetta <f.galetta@reply.it>
2023-11-15 14:50:57 +0100

    Fix_V7_Stored_XSS

--- src/com/notsecurebank/util/OperationsUtil.java
+++ src/com/notsecurebank/util/OperationsUtil.java
@@ -101,7 +101,7 @@ public class OperationsUtil {
 
     public static String sendFeedback(String name, String email, String subject, String comments) {
         LOG.debug("sendFeedback('" + name + "', '" + email + "', '" + subject + "', '" + comments + "')");
-        name = StringEscapeUtils.escapeSql(name).escapeHtml(name);
+        name = StringEscapeUtils.escapeSql(name).escapeHtml(name); 
         email = StringEscapeUtils.escapeSql(email).escapeHtml(email);
         subject = StringEscapeUtils.escapeSql(subject).escapeHtml(subject);
         comments = StringEscapeUtils.escapeSql(comments).escapeHtml(comments);
